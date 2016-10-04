8c10c844eb4a9a0f1b2a1cf1df0e037c77ff0b00
Anton Lunenko <alunenko@n-ix.com>
2016-10-04 23:12:08 +0300

    create main navigation

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -2,7 +2,6 @@
 
 (() => {
     $(document).ready(() => {
-        //menu hover
         var $fixedItems = $('.fixed-items')
         , $hamburger = $('.hamburger')
         , $hamburgerSpan = $('.hamburger span')
@@ -11,16 +10,19 @@
         , $staticItems = $('.static-items')
         , $gifts = $('.gifts');
 
+        $nav.css('height', `calc(${$(window).outerHeight()}px - 106px)`);
+        $hamburger.on('click', () => {
+            $hamburger.toggleClass('open');
+            $nav.toggleClass('open');
+        });
         $hamburger.hover(
             () => { $fixedItems.addClass('m-hamberger-hover'); }
             , () => { $fixedItems.removeClass('m-hamberger-hover'); }
         );
-
         $exhibitions.hover(
             () => { $nav.addClass('m-exebitions-hover'); }
             , () => { $nav.removeClass('m-exebitions-hover'); }
         );
-
         $gifts.hover(
             () => { $staticItems.addClass('m-gifts-hover'); }
             , () => { $staticItems.removeClass('m-gifts-hover'); }
