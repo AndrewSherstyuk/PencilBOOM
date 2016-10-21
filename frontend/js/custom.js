9d03b51c9f9d195a8925a7bdbec558c7d0c4d3ec
Anton Lunenko <alunenko@n-ix.com>
2016-10-21 18:52:56 +0300

    update animation, on gallery_1 change a few images positions

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -36,17 +36,21 @@
             "$border": $('#border_1'),
             "$fish": $('#fish_1'),
             "$fish_wrapper": $('#fish_1_wrapper'),
+            "$viewport": $('#fish_1-viewport'),
             "direction": 'right',
-            "verticalBorderWidth": 20,
-            "additionalMargin": 16
+            "verticalBorderWidth": 25,
+            "additionalMargin": 16,
+            "outWidth": 0
         }
         , block_1_fish_2_Config = {
             "$border": $('#block_1_border_2'),
             "$fish": $('#fish_block_1_2'),
             "$fish_wrapper": $('#fish_block_1_2_wrapper'),
+            "$viewport": $('#fish_block_1_2-viewport'),
             "direction": 'right',
             "verticalBorderWidth": 0,
-            "additionalMargin": -25
+            "additionalMargin": -25,
+            "outWidth": 126
         };
 
         $languagesItems = $languages.find('li');
@@ -90,18 +94,21 @@
 
         /* define size and position of viewport(border) */
         let border_offset = config.$border.offset()
-          , border_width = config.$border.outerWidth();
+          , border_width = config.$border.outerWidth()
+          , both_borders = config.verticalBorderWidth * 2
+          , padding = parseInt($(config.$viewport).css('padding-left'));
 
         let fish_wrapperCss = {
             "top": (border_offset.top - config.$border.closest('.full-width').offset().top) + (config.$border.outerHeight() - config.$fish_wrapper.height()) / 2 + 'px',
-            "left": border_offset.left + config.additionalMargin + 'px',
-            "width": $(window).outerWidth() - border_offset.left - config.additionalMargin + 'px'
+            "right": 0,
+            "width": ($(window).outerWidth() - border_offset.left - both_borders - padding + config.outWidth) * 2 + 'px'
         };
+        $(config.$viewport).css('width', parseInt(fish_wrapperCss.width) / 2 + padding + 'px')
         config.$fish_wrapper.css(fish_wrapperCss);
 
         /* define fish behaviour */
         let fish_width = config.$fish.outerWidth()
-        , animationDuration = 10000
+        , animationDuration = 5000
         , pauseTimeAnimation = 5000
         , stop = {}
         , out = {}
@@ -109,8 +116,9 @@
 
         let stopPointDisagreement = border_width - fish_width;
         let isStopPointDisagreementPossitive = stopPointDisagreement > 0;
-        stop[config.direction] = (stopPointDisagreement / 2 * -1 + ((isStopPointDisagreementPossitive) ? 0 : config.additionalMargin)) + 'px';
-        out[config.direction] = fish_width + 'px';
+        stop[config.direction] = (config.outWidth) ? config.outWidth * -1 : '0%';
+        // stop[config.direction] = ((stopPointDisagreement / 2 - both_borders) * -1 + ((isStopPointDisagreementPossitive) ? 0 : config.additionalMargin)) + 'px';debugger;
+        out[config.direction] = '100%';
         start[config.direction] = '-100%';
 
         runAnimate(config);
